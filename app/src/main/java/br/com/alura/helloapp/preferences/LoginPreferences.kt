4c1508f764861befe0e9bdc21297ed2e5193afa7
Cainã Oliveira <csoliveira01@gmail.com>
2023-09-14 21:02:50 -0400

    Saving user and password in a dataStore

--- app/src/main/java/br/com/alura/helloapp/preferences/LoginPreferences.kt
+++ app/src/main/java/br/com/alura/helloapp/preferences/LoginPreferences.kt
@@ -3,8 +3,14 @@ package br.com.alura.helloapp.preferences
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 
 // At the top level of your kotlin file:
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "login")
+
+object PreferencesKey {
+    val USER = stringPreferencesKey("user")
+    val PASSWORD = stringPreferencesKey("password")
+}
